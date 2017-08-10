be5bbc3f1454c2fb1e51f76d256b0202c4f61180
CaliHoya <CaliHoya@users.noreply.github.com>
2017-08-10 08:57:53 -0700

    New file update

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+# Movie Trailer Website
+
+## Basic Overview
+This project: 
+- Displays some of my favorite movies
+- Plays the trailer of each movie when you click on the movie poster
+
+## Install
+Using your terminal, use the following code to clone this repository to the local destination folder of your choice:
+```python
+git clone https://github.com/CaliHoya/Movie_Trailer_Project.git
+```
+
+## Directions
+To view and explore the Movie Trailer Website, run the following:
+```python
+python entertainment_center.py
+```
+
+### Thank You For Checking This Out!
---
+++ media.py
@@ -0,0 +1,23 @@
+import webbrowser
+
+class Movie():
+    """Creates instances of movies with key data and method to play trailer
+
+    Attributes:
+        title: A string containing movie title
+        storyline: A string containing movie title
+        poster_image_url: URL of the movie poster image file
+        trailer_youtube_url: URL of movie trailer
+    """
+
+    def __init__(self, movie_title, movie_storyline, poster_image, 
+    	         trailer_youtube):
+        """Inits Movie instance with all relevant movie data"""
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+    	"""Opens movie trailer pop-up"""
+        webbrowser.open(self.trailer_youtube_url)
